32c45e7962572e47f8dfa414166f14340b4661be
bandeira-th <trabalho.bandeira@gmail.com>
2024-10-30 12:15:55 -0300

    feat: player attacks

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
+function getRandomValue(max, min) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
 const app = Vue.createApp({
-    data(){
-        return{
-            playerHealth: 100,
-            beastHealth: 100,
-        }
+  data() {
+    return {
+      playerHealth: 100,
+      beastHealth: 100,
+      currentRound: 0,
     }
+  },
+  computed: {
+    isSpecialAttackEnabled() {
+      return this.currentRound % 3 !== 0 || this.currentRound === 0
+    },
+    playerHealthStyles() {
+      return { width: this.playerHealth + "%" }
+    },
+    beastHealthStyles() {
+      return { width: this.beastHealth + "%" }
+    },
+  },
+  methods: {
+    playerAttacks() {
+      const attackValue = getRandomValue(12, 5)
+      this.beastHealth -= attackValue
+      this.beastAttacks()
+      this.currentRound++
+    },
+    playerAttacksSpecial() {
+      const attackValue = getRandomValue(25, 10)
+      this.beastHealth -= attackValue
+      this.beastAttacks()
+      this.currentRound++
+    },
+    beastAttacks() {
+      const attackValue = getRandomValue(15, 8)
+      this.playerHealth -= attackValue
+    },
+  },
 }).mount("#game")
